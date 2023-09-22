9cee08ac95e5c08465077fccaa840545a5ff5399
armarianAT <arodriguez.marian@atsistemas.com>
2023-09-22 13:35:13 +0200

    primer commit

---
+++ src/app/components/CustomHeading.tsx
@@ -0,0 +1,17 @@
+import { defineStyle, defineStyleConfig } from "@chakra-ui/react";
+
+export default defineStyleConfig({
+    // Styles for the base style
+    baseStyle: {
+        font: "arial",
+        fontSize: "1rem"
+
+    },
+    // Styles for the size variations
+    sizes: {},
+    // Styles for the visual style variations
+    variants: {},
+    // The default `size` or `variant` values
+    defaultProps: {},
+});
+
